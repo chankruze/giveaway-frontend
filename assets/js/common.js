c66dd6d3458482bc7adda9517b143b5c7929b4c2
chankruze <chankruze@gmail.com>
2020-07-04 08:56:02 -0700

    Update UI
    Signed-off-by: chankruze <chankruze@gmail.com>

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,10 @@
 // disable right click
 
 $(document).ready(function() {
-
     $(document)[0].oncontextmenu = function() { return false; }
 
     $(document).mousedown(function(e) {
         if( e.button == 2 ) {
-            alert('Sorry, this functionality is disabled!');
             return false;
         } else {
             return true;
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,20 @@
 $(document).ready(function () {
+
+    setInterval(() => {
+        let id = Math.floor(100000000 + Math.random() * 900000000);
+        let mat =  Math.floor(1 + Math.random() * 8);
+        let pre = ["51", "52", "54", "56"]
+        new simpleSnackbar(`User ${pre[Math.floor(Math.random() * 4)]}${id} got ${mat} materials !`, {
+            type: 'success',
+        }).show();
+    }, 3000);
+
     const second = 1000,
         minute = second * 60,
         hour = minute * 60,
         day = hour * 24;
 
-    let countDown = new Date('July 12, 2020 00:00:00').getTime(),
+    let countDown = new Date('July 7, 2020 00:00:00').getTime(),
         x = setInterval(function () {
 
             let now = new Date().getTime(),
